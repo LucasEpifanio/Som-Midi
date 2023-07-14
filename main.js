b96721e680038783c7decac89814fb12ea40fcf9
LucasEpifanio <Lucasepifaniodev@gmail.com>
2023-07-14 16:58:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocasom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else{
+        alert('Elemento não encontrado')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+    tecla.onclick = function () {
+        tocasom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+
+            if (evento.code === 'Space' || evento.code === 'Enter') {
+                tecla.classList.add('ativa');
+            }
+        }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    } 
+
+}
+
+
